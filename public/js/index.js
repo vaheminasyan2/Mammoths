f9de857dac4d87113344c7b3c634d3bf547af315
curtisyungen <39065500+curtisyungen@users.noreply.github.com>
2019-02-02 22:36:29 -0800

    added btn functionality, basic activity forms

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,6 @@ var $exampleDescription = $("#example-description");
 var $submitBtn = $("#submit");
 var $exampleList = $("#example-list");
 
-
 // The API object contains methods for each kind of request we'll make
 var API = {
   saveExample: function(example) {
@@ -94,6 +93,10 @@ var handleDeleteBtnClick = function() {
     refreshExamples();
   });
 };
+
+function getPage() {
+  
+}
 
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,9 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -11,25 +11,50 @@ module.exports = function(app) {
     });
   });
 
-  app.get("/login", function(req, res) {
-      res.render("login")
+  app.get("/login", function (req, res) {
+    res.render("login");
   });
 
-  app.get("/register", function(req, res) {
-      res.render("register")
+  app.get("/register", function (req, res) {
+    res.render("register");
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
     });
   });
 
+  // Get Post Activity page
+  app.get("/post-activity", function (req, res) {
+    res.render("post-activity");
+  });
+
+  // Get Plan page
+  app.get("/plan", function (req, res) {
+    res.render("plan");
+  });
+
+  // Get History page
+  app.get("/history", function (req, res) {
+    res.render("history");
+  });
+
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
+  });
+
+
+  app.get("/goTo", function(req, res) {
+    res.render(req.params.page);
+  });
+
+  // Submit new activity to database
+  app.post("/", function(req, res) {
+    res.render("index");
   });
 };
