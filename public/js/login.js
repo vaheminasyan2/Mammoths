82dbc9596c5165c0eb596faeadbe025f28088128
Vahe Minasyan (Red Door Collaborative LLC) <v-vamin@microsoft.com>
2019-02-17 00:30:49 -0800

    updates

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,10 @@
+
+
 function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
   var id_token = googleUser.getAuthResponse().id_token;
   var xhr = new XMLHttpRequest();
   xhr.open('POST', '/api/login');
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  //var user = 
   xhr.onload = function () {
     var googleUserObject = JSON.parse(xhr.responseText);
     localStorage.setItem("userId", googleUserObject.sub);
